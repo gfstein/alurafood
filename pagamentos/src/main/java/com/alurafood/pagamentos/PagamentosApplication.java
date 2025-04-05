fc7aab60fafc587c56e11b530c7112de607f8b9b
Guilherme Stein <guilhermefloresstein@gmail.com>
2025-04-05 02:56:18 -0300

    Adiciona serviço gateway com Spring Cloud Gateway
    Configura um serviço de gateway utilizando Spring Cloud Gateway e Eureka Client. Inclui arquivos necessários, como `pom.xml`, `.gitignore`, e as classes base da aplicação.

--- pagamentos/src/main/java/com/alurafood/pagamentos/PagamentosApplication.java
+++ pagamentos/src/main/java/com/alurafood/pagamentos/PagamentosApplication.java
@@ -2,13 +2,8 @@ package com.alurafood.pagamentos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
-
-import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class PagamentosApplication {
 
     public static void main(String[] args) {
